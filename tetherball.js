7dbab792f6b067f38dd66166d841a330117b2953
Scott Silvi <scottsilvi@gmail.com>
2012-11-03 14:39:57 -0700

    Adding routing

--- tetherball.js
+++ tetherball.js
@@ -1,4 +1,28 @@
 if (Meteor.isClient) {
+
+
+
+var AppRouter = Backbone.Router.extend({
+        routes: {
+            "posts/:id": "getPost",
+            "*actions": "defaultRoute" // Backbone will try match the route above first
+        }
+    });
+    // Instantiate the router
+    var app_router = new AppRouter;
+    app_router.on('route:getPost', function (id) {
+        // Note the variable in the route definition being passed in here
+        alert( "Get post number " + id );   
+    });
+    app_router.on('route:defaultRoute', function (actions) {
+        alert( actions ); 
+    });
+
+    // Start Backbone history a necessary step for bookmarkable URL's
+    Backbone.history.start();
+
+
+  
   Template.hello.greeting = function () {
     return "Welcome to tetherball.";
   };
