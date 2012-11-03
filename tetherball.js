8ba3bcc95e8f4c449804283ef43c47c91db3a5df
Daniel Coffman <dgcoffman@gmail.com>
2012-11-03 15:50:58 -0700

    Routing somewhat figured out

--- tetherball.js
+++ tetherball.js
@@ -1,43 +1,38 @@
 if (Meteor.isClient) {
 
+Template.home.selected = function () {
+  return Session.equals('currentView', 'home');
+};
 
+Template.about.selected = function () {
+  return Session.equals('currentView', 'about');
+};
 
-var AppRouter = Backbone.Router.extend({
+Template.contact.selected = function () {
+  return Session.equals('currentView', 'contact');
+};
+
+Template.login.selected = function () {
+  return Session.equals('currentView', 'login');
+};
+
+var Router = Backbone.Router.extend({
         routes: {
-            "posts/:id": "getPost",
             "*actions": "defaultRoute" // Backbone will try match the route above first
         }
     });
+
     // Instantiate the router
-    var app_router = new AppRouter;
-    app_router.on('route:getPost', function (id) {
-        // Note the variable in the route definition being passed in here
-        alert( "Get post number " + id );   
-    });
+    var app_router = new Router();
+
     app_router.on('route:defaultRoute', function (actions) {
-        alert( actions ); 
+        Session.set("currentView", actions);
+        this.navigate(actions, true);
     });
 
-    // Start Backbone history a necessary step for bookmarkable URL's
     Backbone.history.start();
 
-
-  
-  Template.hello.greeting = function () {
-    return "Welcome to tetherball.";
-  };
-
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
-}
-
-if (Meteor.isServer) {
   Meteor.startup(function () {
-    // code to run on server at startup
+    
   });
 }
